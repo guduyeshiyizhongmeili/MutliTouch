325389d129da744238fb67d22c447cc909f2e5b7
liuhaifeng <1932736402@qq.com>
2017-09-29 09:39:06 +0800

    Initial commit

---
+++ app/src/main/java/com/baway/mutlitouch/CircleView.java
@@ -0,0 +1,97 @@
+package com.baway.mutlitouch;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by peng on 2017/9/4.
+ */
+
+public class CircleView extends View {
+    private float circleX;
+    private float circleY;
+    private float radius;
+    private Paint paint;
+    private final int DRAG = 1;//拖拽
+    private final int ZOOM = 2;//缩放
+    private final int IDLE = 3;//空闲状态
+    private int mode = IDLE;
+    private double startDis;//起始时，两个手指的距离
+    private float resultDis;
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        //获取属性值
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        circleX = a.getDimension(R.styleable.CircleView_circleX, 50);
+        circleY = a.getDimension(R.styleable.CircleView_circleY, 50);
+        radius = a.getDimension(R.styleable.CircleView_radius, 50);
+        a.recycle();
+
+        //初始化paint
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(circleX, circleY, radius + resultDis, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                mode = ZOOM;
+                Log.e("CircleView", "ZOOM");
+                //获取两个点的x y
+                float px1 = event.getX(0);
+                float px2 = event.getX(1);
+                float py1 = event.getY(0);
+                float py2 = event.getY(1);
+                float distanceX = px2 - px1;
+                float distanceY = py2 - py1;
+                startDis = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    circleX = event.getX();
+                    circleY = event.getY();
+                    invalidate();
+                } else {
+                    float mpx1 = event.getX(0);
+                    float mpx2 = event.getX(1);
+                    float mpy1 = event.getY(0);
+                    float mpy2 = event.getY(1);
+                    float mdistanceX = mpx2 - mpx1;
+                    float mdistanceY = mpy2 - mpy1;
+                    double endDis = Math.sqrt(mdistanceX * mdistanceX + mdistanceY * mdistanceY);
+                    resultDis = (float) (endDis - startDis);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mode = IDLE;
+                break;
+        }
+        return true;
+    }
+}
